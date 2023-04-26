cdd738b54e0583d7cbeac9966ebcbba2d46cfc96
bones <RAM@RRRR.com>
2023-04-26 13:14:48 +0530

    Added scripted pipline

--- jenkinsfile.js
+++ jenkinsfile.js
@@ -1,6 +1,6 @@
 node('MAVEN_JDK8') {
     stage('version control') {
-        git url: 'https://github.com/Bonesumil/game-of-life.git',
+        git url: 'https://github.com/Bonesumil/game-of-life.git'
             branch: 'scripte'
    }
 }
